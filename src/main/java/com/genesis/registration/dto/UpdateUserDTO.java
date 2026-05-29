55c099a026960f6a6d4fa76a8e6e2f340ce82eba
rhrabovs <classo69@gmail.com>
2026-05-29 14:57:02 +0200

    init

---
+++ src/main/java/com/genesis/registration/dto/UpdateUserDTO.java
@@ -0,0 +1,51 @@
+package com.genesis.registration.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public class UpdateUserDTO {
+
+    @NotNull
+    private Long id;
+
+    @NotBlank
+    @Size(min = 2, max = 50)
+    private String name;
+
+    @NotBlank
+    @Size(min = 2, max = 50)
+    private String surname;
+
+    public UpdateUserDTO() {
+    }
+
+
+
+
+    // getter & setter
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+}
