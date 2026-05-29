1a6f388dce7817fc6d218944c52e4125c8acdec3
rhrabovs <classo69@gmail.com>
2026-05-29 16:51:15 +0200

    prejmenovano, pro automaticke spusteni

--- src/main/resources/static/schema.sql
+++ src/main/resources/static/schema.sql
@@ -8,7 +8,3 @@ CREATE TABLE users (
                        uuid VARCHAR(255) NOT NULL UNIQUE
 );
 
--- test insertu
-
-INSERT INTO users (name, surname, person_id, uuid)
-VALUES ('Jan', 'Novak', 'vB1fX4rH7iNt', '11111111-2222-3333-4444-555555555555');
